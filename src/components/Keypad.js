bf8c7c5c631140283a72bf6b2d687dad9519c076
Mary <omota4fidelity@gmail.com>
2023-09-03 18:43:55 +0100

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(e){
+        console.log(e.target.name)
+    }
     return (
-        <div></div>
+        <div>
+            <h6>Please Enter your password</h6>
+            <input 
+            type="password" 
+            name="Entering password..."
+            onChange={handleChange}
+            placeholder="Entering password..."
+            />
+        </div>
     )
 }
 
